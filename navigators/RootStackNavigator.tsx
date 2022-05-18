33cbae10db935127a3d65876fb9a1b48b2ad3551
Swarnendu Chatterjee <swarnendubabai32@gmail.com>
2022-05-18 14:55:30 +0530

    tested

--- navigators/RootStackNavigator.tsx
+++ navigators/RootStackNavigator.tsx
@@ -4,30 +4,39 @@ import { EditPhoto } from "../screens/EditPhoto";
 import { PhotoUploadDetails } from "../screens/PhotoUploadDetails";
 import { SelectedLocalPhotoPreview } from "../screens/SelectedLocalPhotoPreview";
 import { MediaParams } from "../constants/types";
+import { TempScreen } from "../screens/TempScreen";
 
 export type RootStackNavigationParams = {
-  CreateContent: undefined;
-  SelectedLocalPhotoPreview: {
-    list: MediaParams[];
-  };
-  EditPhoto: undefined;
-  PhotoUploadDetails: {
-    list: MediaParams[];
-  };
+    CreateContent: undefined;
+    SelectedLocalPhotoPreview: {
+        list: MediaParams[];
+    };
+    EditPhoto: undefined;
+    PhotoUploadDetails: {
+        list: MediaParams[];
+    };
+    TempScreen: undefined;
 };
 
 const Tab = createNativeStackNavigator<RootStackNavigationParams>();
 
 export function RootStackNavigator() {
-  return (
-    <Tab.Navigator screenOptions={{ header: () => null }}>
-      <Tab.Screen name="CreateContent" component={CreateContent} />
-      <Tab.Screen
-        name="SelectedLocalPhotoPreview"
-        component={SelectedLocalPhotoPreview}
-      />
-      <Tab.Screen name="EditPhoto" component={EditPhoto} />
-      <Tab.Screen name="PhotoUploadDetails" component={PhotoUploadDetails} />
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator
+            screenOptions={{ header: () => null }}
+            initialRouteName="TempScreen"
+        >
+            <Tab.Screen name="TempScreen" component={TempScreen} />
+            <Tab.Screen name="CreateContent" component={CreateContent} />
+            <Tab.Screen
+                name="SelectedLocalPhotoPreview"
+                component={SelectedLocalPhotoPreview}
+            />
+            <Tab.Screen name="EditPhoto" component={EditPhoto} />
+            <Tab.Screen
+                name="PhotoUploadDetails"
+                component={PhotoUploadDetails}
+            />
+        </Tab.Navigator>
+    );
 }
---
+++ screens/TempScreen.tsx
@@ -0,0 +1,173 @@
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import { AppIcon } from "../components/utility/AppIcon";
+
+export const TempScreen = () => {
+    return (
+        <ScrollView style={{ flex: 1, alignSelf: "stretch" }}>
+            <AppIcon name="user-outline" size="medium" foreground="#000" />
+            <AppIcon name="user-solid" size="medium" foreground="#000" />
+            <AppIcon name="volume-high" size="medium" foreground="#000" />
+            <AppIcon name="volume-medium" size="medium" foreground="#000" />
+            <AppIcon name="volume-low" size="medium" foreground="#000" />
+            <AppIcon name="added-to-playlist" size="medium" foreground="#000" />
+            <AppIcon name="add-song" size="medium" foreground="#000" />
+            <AppIcon name="add-to-playlist" size="medium" foreground="#000" />
+            <AppIcon name="arrow-right" size="medium" foreground="#000" />
+            <AppIcon name="arrow-up" size="medium" foreground="#000" />
+            <AppIcon name="caption-outline" size="medium" foreground="#000" />
+            <AppIcon name="caption-solid" size="medium" foreground="#000" />
+            <AppIcon name="chevron-down" size="medium" foreground="#000" />
+            <AppIcon name="chevron-left" size="medium" foreground="#000" />
+            <AppIcon name="chevron-right" size="medium" foreground="#000" />
+            <AppIcon name="chevron-up" size="medium" foreground="#000" />
+            <AppIcon name="edit-outline" size="medium" foreground="#000" />
+            <AppIcon name="close" size="medium" foreground="#000" />
+            <AppIcon name="mute" size="medium" foreground="#000" />
+            <AppIcon name="tick" size="medium" foreground="#000" />
+            <AppIcon name="tag-regular" size="medium" foreground="#000" />
+            <AppIcon name="tag-bold" size="medium" foreground="#000" />
+            <AppIcon name="show" size="medium" foreground="#000" />
+            <AppIcon name="share-solid" size="medium" foreground="#000" />
+            <AppIcon name="share-outline" size="medium" foreground="#000" />
+            <AppIcon name="settings-solid" size="medium" foreground="#000" />
+            <AppIcon name="settings-outline" size="medium" foreground="#000" />
+            <AppIcon name="send" size="medium" foreground="#000" />
+            <AppIcon name="vibrance" size="medium" foreground="#000" />
+            <AppIcon name="search-regular" size="medium" foreground="#000" />
+            <AppIcon name="search-bold" size="medium" foreground="#000" />
+            <AppIcon name="previous" size="medium" foreground="#000" />
+            <AppIcon name="phone-solid" size="medium" foreground="#000" />
+            <AppIcon name="report" size="medium" foreground="#000" />
+            <AppIcon name="heart-solid" size="medium" foreground="#000" />
+            <AppIcon name="history" size="medium" foreground="#000" />
+            <AppIcon name="home-solid" size="medium" foreground="#000" />
+            <AppIcon name="live" size="medium" foreground="#000" />
+            <AppIcon name="location" size="medium" foreground="#000" />
+            <AppIcon name="maximize" size="medium" foreground="#000" />
+            <AppIcon name="mention" size="medium" foreground="#000" />
+            <AppIcon name="minimize" size="medium" foreground="#000" />
+            <AppIcon name="more" size="medium" foreground="#000" />
+            <AppIcon name="hashtag" size="medium" foreground="#000" />
+            <AppIcon name="group-solid" size="medium" foreground="#000" />
+            <AppIcon name="group-outline" size="medium" foreground="#000" />
+            <AppIcon name="following" size="medium" foreground="#000" />
+            <AppIcon name="folder" size="medium" foreground="#000" />
+            <AppIcon name="filter" size="medium" foreground="#000" />
+            <AppIcon name="edit-solid" size="medium" foreground="#000" />
+            <AppIcon name="next" size="medium" foreground="#000" />
+            <AppIcon
+                name="notification-outline"
+                size="medium"
+                foreground="#000"
+            />
+            <AppIcon
+                name="notification-solid"
+                size="medium"
+                foreground="#000"
+            />
+            <AppIcon name="paperclip" size="medium" foreground="#000" />
+            <AppIcon name="pause" size="medium" foreground="#000" />
+            <AppIcon name="phone-outline" size="medium" foreground="#000" />
+            <AppIcon name="trending-outline" size="medium" foreground="#000" />
+            <AppIcon name="trending-solid" size="medium" foreground="#000" />
+            <AppIcon name="video-outline" size="medium" foreground="#000" />
+            <AppIcon name="around-the-clock" size="medium" foreground="#000" />
+            <AppIcon name="arrow-down" size="medium" foreground="#000" />
+            <AppIcon name="arrow-left" size="medium" foreground="#000" />
+            <AppIcon name="follow" size="medium" foreground="#000" />
+            <AppIcon name="video-solid" size="medium" foreground="#000" />
+            <AppIcon name="message-solid" size="medium" foreground="#000" />
+            <AppIcon name="comment-outline" size="medium" foreground="#000" />
+            <AppIcon name="comment-solid" size="medium" foreground="#000" />
+            <AppIcon name="spinner" size="medium" foreground="#000" />
+            <AppIcon name="moon" size="medium" foreground="#000" />
+            <AppIcon name="sun" size="medium" foreground="#000" />
+            <AppIcon name="message-outline" size="medium" foreground="#000" />
+            <AppIcon name="heart-outline" size="medium" foreground="#000" />
+            <AppIcon name="trash" size="medium" foreground="#000" />
+            <AppIcon name="mic-on" size="medium" foreground="#000" />
+            <AppIcon name="mic-off" size="medium" foreground="#000" />
+            <AppIcon name="mute" size="medium" foreground="#000" />
+            <AppIcon name="info" size="medium" foreground="#000" />
+            <AppIcon name="home-outline" size="medium" foreground="#000" />
+            <AppIcon name="bookmark-outline" size="medium" foreground="#000" />
+            <AppIcon name="bookmark-solid" size="medium" foreground="#000" />
+            <AppIcon
+                name="video-recorder-outline"
+                size="medium"
+                foreground="#000"
+            />
+            <AppIcon
+                name="video-recorder-solid"
+                size="medium"
+                foreground="#000"
+            />
+            <AppIcon name="camera-outline" size="medium" foreground="#000" />
+            <AppIcon name="camera-solid" size="medium" foreground="#000" />
+            <AppIcon name="hide" size="medium" foreground="#000" />
+            <AppIcon name="play" size="medium" foreground="#000" />
+            <AppIcon name="create" size="medium" foreground="#000" />
+            <AppIcon name="font" size="medium" foreground="#000" />
+            <AppIcon name="music" size="medium" foreground="#000" />
+            <AppIcon name="boomerang" size="medium" foreground="#000" />
+            <AppIcon name="download" size="medium" foreground="#000" />
+            <AppIcon name="favourite" size="medium" foreground="#000" />
+            <AppIcon name="sticker" size="medium" foreground="#000" />
+            <AppIcon name="doodle" size="medium" foreground="#000" />
+            <AppIcon name="flash-off" size="medium" foreground="#000" />
+            <AppIcon name="flash-on" size="medium" foreground="#000" />
+            <AppIcon name="multiselect" size="medium" foreground="#000" />
+            <AppIcon name="camera-flip" size="medium" foreground="#000" />
+            <AppIcon name="pin-outline" size="medium" foreground="#000" />
+            <AppIcon name="pin-solid" size="medium" foreground="#000" />
+            <AppIcon name="dehaze" size="medium" foreground="#000" />
+            <AppIcon name="tint" size="medium" foreground="#000" />
+            <AppIcon name="clarity" size="medium" foreground="#000" />
+            <AppIcon name="contrast" size="medium" foreground="#000" />
+            <AppIcon name="edge-brush" size="medium" foreground="#000" />
+            <AppIcon name="extra-thin-brush" size="medium" foreground="#000" />
+            <AppIcon name="equalizer" size="medium" foreground="#000" />
+            <AppIcon name="expand" size="medium" foreground="#000" />
+            <AppIcon name="exposure" size="medium" foreground="#000" />
+            <AppIcon name="eraser" size="medium" foreground="#000" />
+            <AppIcon name="fill" size="medium" foreground="#000" />
+            <AppIcon name="filter" size="medium" foreground="#000" />
+            <AppIcon name="flash-off" size="medium" foreground="#000" />
+            <AppIcon name="flash-on" size="medium" foreground="#000" />
+            <AppIcon name="font" size="medium" foreground="#000" />
+            <AppIcon name="gif" size="medium" foreground="#000" />
+            <AppIcon name="glamour" size="medium" foreground="#000" />
+            <AppIcon name="grain" size="medium" foreground="#000" />
+            <AppIcon name="grid" size="medium" foreground="#000" />
+            <AppIcon name="help" size="medium" foreground="#000" />
+            <AppIcon name="highlight" size="medium" foreground="#000" />
+            <AppIcon name="hue" size="medium" foreground="#000" />
+            <AppIcon name="layout" size="medium" foreground="#000" />
+            <AppIcon name="layout-1" size="medium" foreground="#000" />
+            <AppIcon name="layout-2" size="medium" foreground="#000" />
+            <AppIcon name="layout-3" size="medium" foreground="#000" />
+            <AppIcon name="lock" size="medium" foreground="#000" />
+            <AppIcon name="multicapture" size="medium" foreground="#000" />
+            <AppIcon name="saturation" size="medium" foreground="#000" />
+            <AppIcon name="security" size="medium" foreground="#000" />
+            <AppIcon name="pin-outline" size="medium" foreground="#000" />
+            <AppIcon name="sharp" size="medium" foreground="#000" />
+            <AppIcon name="shinny" size="medium" foreground="#000" />
+            <AppIcon name="shorts" size="medium" foreground="#000" />
+            <AppIcon name="smiley" size="medium" foreground="#000" />
+            <AppIcon name="about" size="medium" foreground="#000" />
+            <AppIcon name="temperature" size="medium" foreground="#000" />
+            <AppIcon name="thin-brush" size="medium" foreground="#000" />
+            <AppIcon name="timer-15" size="medium" foreground="#000" />
+            <AppIcon name="timer-30" size="medium" foreground="#000" />
+            <AppIcon name="timer-60" size="medium" foreground="#000" />
+            <AppIcon name="tint" size="medium" foreground="#000" />
+            <AppIcon name="tone" size="medium" foreground="#000" />
+            <AppIcon name="undo" size="medium" foreground="#000" />
+            <AppIcon name="vignette" size="medium" foreground="#000" />
+            <AppIcon name="pin-outline" size="medium" foreground="#000" />
+            <AppIcon name="white" size="medium" foreground="#000" />
+        </ScrollView>
+    );
+};
